cbc61c895a6f8faf7c2bfb8437fc174311a3ae6d
nrochan3 <82353252+nrochan3@users.noreply.github.com>
2021-04-26 00:28:57 +0000

    Add files via upload

---
+++ Migrations/202104111827365_IEnumerableGender.cs
@@ -0,0 +1,16 @@
+namespace AdmissionsWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IEnumerableGender : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
